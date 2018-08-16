4ef23464e120872053e650e9d64741ec59f0e469
TheFrost <frost084@gmail.com>
2018-08-16 03:00:19 -0500

    Build log structure module

---
+++ src/js/log.js
@@ -0,0 +1,26 @@
+const logger = {
+  log: [],
+  currentLog: {
+    interactions: [],
+    level: 1,
+    totalShapePixels: 0
+  },
+  currentInteraction: {
+    x0: 0,
+    y0: 0,
+    x1: 0,
+    y1: 0,
+    delta0: 0,
+    delta1: 0
+  },
+
+  saveInteractionState() {
+    this.currentLog.interactions.push({...this.currentInteraction});
+  },
+  saveLogState() {
+    this.log.push({...this.currentLog});
+    this.currentLog.interactions = [];
+  }
+};
+
+export default logger;
